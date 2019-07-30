c2713fa19e8c25ce16c0a1d39ccfba96db2a4ee7
MicahJank <MicahJank@users.noreply.github.com>
2019-07-30 14:35:25 -0500

    switched out the axios get request with a placeholder img that i can use so i dont use up all my requests

--- src/componenets/main/POTD.js
+++ src/componenets/main/POTD.js
@@ -2,28 +2,32 @@ import React, { useState, useEffect} from "react";
 
 import axios from "axios";
 
+import placeHolderImg from '../../placeholder.jpg';
+
 
 const PictureOfTheDay = () => {
 
     const [picture, setPicture] = useState('');
 
-    useEffect(() => {
-        axios
-            .get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+    // useEffect(() => {
+    //     axios
+    //         .get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
 
-            .then(imgObject => {
-                setPicture(imgObject.data.url)
-                console.log(imgObject.data);
-            })
+    //         .then(imgObject => {
+    //             setPicture(imgObject.data.url)
+    //             console.log(imgObject.data);
+    //         })
 
-            .catch(err => {
-                alert(err);
-            });
-    }, []);
+    //         .catch(err => {
+    //             alert(err);
+    //         });
+    // }, []);
+
+    // TODO uncommment above and remove setPicture below
 
     return (
         <div className="main-picture">
-            <img src={picture} alt='From Nasa' />
+            <img src={placeHolderImg} alt='From Nasa' />
         </div>
     )
 };
